d015695aebe650127a6edeae59bf06a9397ba4e2
Ben Cherry <ben@livekit.io>
2025-02-20 22:07:43 -0800

    sample

--- swift-frontend/BroadcastExtension/SampleHandler.swift
+++ swift-frontend/BroadcastExtension/SampleHandler.swift
@@ -1,4 +1,8 @@
 import LiveKit
-import ReplayKit
 
-class SampleHandler: LKSampleHandler {}
+#if os(iOS)
+@available(macCatalyst 13.1, *)
+class SampleHandler: LKSampleHandler {
+    override var enableLogging: Bool { true }
+}
+#endif
