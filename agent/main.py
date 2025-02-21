ad46f2df6c67ff0773752e37de864c4e0f42b85b
Ben Cherry <ben@livekit.io>
2025-02-20 20:01:07 -0800

    wip

--- agent/main.py
+++ agent/main.py
@@ -8,7 +8,7 @@ from livekit.agents import (
     cli,
 )
 from livekit.agents.pipeline import PipelineAgent
-from livekit.plugins import openai
+from livekit.plugins import google
 
 load_dotenv()
 
@@ -19,13 +19,19 @@ logger.setLevel(logging.INFO)
 async def entrypoint(ctx: JobContext):
     agent = PipelineAgent(
         instructions="You are a helpful assistant that can see, hear, and speak.",
-        llm=openai.realtime.RealtimeModel(),
+        # llm=openai.realtime.RealtimeModel(),
+        llm=google.beta.realtime.RealtimeModel(
+            model="gemini-2.0-flash-exp",  # Default model
+            modalities="AUDIO",  # Enable audio responses
+            voice="Puck",  # Default voice
+            instructions="You are a helpful assistant that can see, hear, and speak.",
+            temperature=0.7,  # Controls randomness
+        )
     )
 
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
     # participant = await ctx.wait_for_participant()
     await agent.start(room=ctx.room)
-
 
 if __name__ == "__main__":
     cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
